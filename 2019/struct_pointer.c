4220b63469dd847b1d35d10c058d6bcef56b493d
mio256 <reiya@woorth.io>
2023-02-14 23:11:59 +0900

    :fire:

---
+++ 2019/struct_pointer.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct DATA{
+    int height;
+    int weight;
+};
+
+int main(void){
+    struct DATA *p_data;
+    struct DATA data;
+
+    data.height=190;
+    data.weight=65;
+
+    printf("data.height=%d\n",data.height);
+    printf("data.weight=%d\n",data.weight);
+
+    p_data=&data;
+
+    printf("p_data.height=%d\n",p_data->height);
+    printf("p_data.weight=%d\n",p_data->weight);
+
+    return 0;
+}
