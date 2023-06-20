ade4e501917f5dcffdb1b870674fe0c7ee836f7f
mio256 <reiya@woorth.io>
2023-06-20 09:00:16 +0900

    :fire:

--- adt/arraystack.h
+++ adt/arraystack.h
@@ -1,16 +1,18 @@
-#define	TRUE 1
+#define TRUE 1
 #define FALSE 0
 
 #define MAXSTACK 100
 
-typedef struct stack {
-    int data[MAXSTACK] ;
-    int index ;
-} STACK ;
+/* スタックの利用者は、この構造体の中身を直接操作してはならない */
+typedef struct stack
+{
+    int data[MAXSTACK];
+    int index;
+} STACK;
 
-STACK *newStack(void) ;
-void stackPush(STACK *sp, int v) ;
-int stackPop(STACK *sp) ;
-int stackElement(STACK *sp) ;
-int stackIsEmpty(STACK *sp) ;
-int stackIsFull(STACK *sp) ;
+STACK *newStack(void);
+void stackPush(STACK *sp, int v);
+int stackPop(STACK *sp);
+int stackElement(STACK *sp);
+int stackIsEmpty(STACK *sp);
+int stackIsFull(STACK *sp);
