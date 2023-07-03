f835e3dfb44bd81ddf0764c722600196aceff7e9
mio256 <reiya@woorth.io>
2023-07-04 08:59:39 +0900

    :fire:

---
+++ integrated_exercises/arstack.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include "arstack.h"
+
+RASTACK *newRAStack(void)
+{
+    RASTACK *sp;
+
+    sp = (RASTACK *)malloc(sizeof(RASTACK));
+    sp->index = 0;
+
+    return sp;
+}
+
+void rastackPush(RASTACK *sp, RATIONAL v)
+{
+    sp->data[sp->index++] = v;
+}
+
+RATIONAL rastackPop(RASTACK *sp)
+{
+    return sp->data[--sp->index];
+}
+
+int rastackElement(RASTACK *sp)
+{
+    return sp->index;
+}
+
+int rastackIsEmpty(RASTACK *sp)
+{
+    if (sp->index <= 0)
+        return TRUE;
+    else
+        return FALSE;
+}
+
+int rastackIsFull(RASTACK *sp)
+{
+    if (sp->index >= MAXRASTACK)
+        return TRUE;
+    else
+        return FALSE;
+}
