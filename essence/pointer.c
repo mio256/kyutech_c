ddf11fe5bd908d2a4fa68123ee74cd53fdf3362d
mio256 <reiya@woorth.io>
2023-07-07 21:32:53 +0900

    :fire:

--- essence/pointer.c
+++ essence/pointer.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+void swap(int *x, int *y)
+{
+    int t;
+
+    t = *x;
+    *x = *y;
+    *y = t;
+}
+
 int main(void)
 {
     int a;
@@ -14,4 +23,19 @@ int main(void)
     printf("p=%p\n", p);   // pの中身
     printf("&p=%p\n", &p); // pのアドレス
     printf("*p=%d\n", *p); // pの中身（aのアドレス）の中身 = pの先
+
+    *p = 10;
+    printf("a=%d\n", a); // aの中身が変わっている
+
+    int x, y, t;
+    x = 6;
+    y = 5;
+    printf("x=%d, y=%d\n", x, y);
+
+    // xとyのアドレス
+    printf("&x=%p\n", &x);
+    printf("&y=%p\n", &y);
+
+    swap(&x, &y); // xとyの値を入れ替える
+    printf("x=%d, y=%d\n", x, y);
 }
